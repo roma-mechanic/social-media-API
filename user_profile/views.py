22e4b943e3cdc52f602c1eedd8dfbaac78307492
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-09-21 19:36:31 +0300

    Fixed problems with the user deleting someone else's profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -34,5 +34,8 @@ class UserProfileUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserProfileDetailSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
