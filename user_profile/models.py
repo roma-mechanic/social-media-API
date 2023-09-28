b265d00dd33add057f36e40fd4f9c59e981a300a
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-09-28 10:21:09 +0300

    removed fiels "email", "first name" "last name" from user profile model, these were duplicates of the model "user".

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,10 +1,13 @@
 import os
 import uuid
 
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.text import slugify
 
 from social_media_api import settings
+
+USER = get_user_model()
 
 
 def movie_image_file_path(instance, filename):
@@ -20,10 +23,7 @@ class UserProfile(models.Model):
         on_delete=models.CASCADE,
         related_name="profile",
     )
-    email = models.EmailField(max_length=100)
     username = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=100, null=True, blank=True)
-    last_name = models.CharField(max_length=100, null=True, blank=True)
     bio = models.TextField(max_length=500, blank=True)
     profile_image = models.ImageField(
         upload_to=movie_image_file_path, blank=True
